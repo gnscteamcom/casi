098a7a74d45013358ae9dc145bf545eb47b69c0f
unknown <willybc06@gmail.com>
2019-12-03 19:09:49 -0300

    Actualizacion de precios

--- grangeillegal/config.lua
+++ grangeillegal/config.lua
@@ -3,14 +3,18 @@ Config = {}
 Config.ArgentSale = true  --true if use of black money, false for regular money
 
 Config.Weapons = {
-    {weapon = "WEAPON_CROWBAR", price = 115},
-    {weapon = "WEAPON_DAGGER", price = 200},
-    {weapon = "WEAPON_MOLOTOV", price = 2750},    
-    {weapon = "WEAPON_PISTOL50", price = 12800},
-    {weapon = "WEAPON_DOUBLEACTION", price = 16200},
-    {weapon = "WEAPON_SAWNOFFSHOTGUN", price = 22400},
-    {weapon = "WEAPON_MINISMG", price = 24800},
-    {weapon = "WEAPON_GUSENBERG", price = 42500}, 
+    {weapon = "WEAPON_CROWBAR", price = 10000},
+    {weapon = "WEAPON_SWITCHBLADE", price = 10000},
+    {weapon = "WEAPON_DAGGER", price = 10000},
+    {weapon = "WEAPON_MOLOTOV", price = 30000},
+    {weapon = "WEAPON_COMBATPISTOL", price = 150000},   
+    {weapon = "WEAPON_PISTOL50", price = 250000},
+    {weapon = "WEAPON_PUMPSHOTGUN", price = 250000},
+    {weapon = "WEAPON_MICROSMG", price = 350000},
+    {weapon = "WEAPON_ASSAULTSMG", price = 400000},
+    {weapon = "WEAPON_ASSAULTRIFLE", price = 500000},
+    {weapon = "WEAPON_ASSAULTRIFLE_MK2", price = 750000},
+
 }
 
 --Weapon List : https://wiki.rage.mp/index.php?title=Weapons
