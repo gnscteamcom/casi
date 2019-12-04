58640cb950e9cbd899bf63c5f984edfd545a584a
unknown <willybc06@gmail.com>
2019-12-03 21:43:26 -0300

    Venta de auto por contrato

---
+++ [esx]/esx_contract/locales/en.lua
@@ -0,0 +1,8 @@
+Locales ['en'] = {
+  ['writingcontract'] = 'Contrato para la siguiente matrícula: %s',
+  ['soldvehicle'] = 'Usted vendió el vehículo con el número de registro. ~r~%s~s~',
+  ['boughtvehicle'] = 'Usted compró el vehículo con el número de registro. ~g~%s~s~',
+  ['notyourcar'] = 'Este no es tu vehículo',
+  ['nonearby'] = 'No hay vehículo cerca',
+  ['nonearbybuyer'] = 'No hay comprador cerca',
+}
